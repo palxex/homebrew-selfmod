d07c3e9cb70646217b96611aa808e6eeb6bb156f
palxex <ex+wsl@palx.org>
2020-09-29 21:33:25 +0800

    obfuscated-openssh: upgrade to 8.3

--- Formula/obfuscated-openssh.rb
+++ Formula/obfuscated-openssh.rb
@@ -1,10 +1,10 @@
 class ObfuscatedOpenssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools with obfuscated patch"
   homepage "https://www.openssh.com/"
-  url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.1p1.tar.gz"
-  mirror "https://mirror.vdms.io/pub/OpenBSD/OpenSSH/portable/openssh-8.1p1.tar.gz"
-  version "8.1p1"
-  sha256 "02f5dbef3835d0753556f973cd57b4c19b6b1f6cd24c03445e23ac77ca1b93ff"
+  url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.3p1.tar.gz"
+  mirror "https://mirror.vdms.io/pub/OpenBSD/OpenSSH/portable/openssh-8.3p1.tar.gz"
+  version "8.3p1"
+  sha256 "f2befbe0472fe7eb75d23340eb17531cb6b3aac24075e2066b41f814e12387b2"
 
   conflicts_with "openssh",
     :because => "both install ssh scp sshd etc"
@@ -14,10 +14,15 @@ class ObfuscatedOpenssh < Formula
 
   depends_on "pkg-config" => :build
   depends_on "ldns"
+  depends_on "libfido2"
   depends_on "openssl@1.1"
-  depends_on "krb5" unless OS.mac?
-  depends_on "linux-pam" unless OS.mac?
-  depends_on "libedit" unless OS.mac?
+  
+  unless OS.mac?
+    depends_on "libedit"
+    depends_on "krb5"
+    depends_on "zlib"
+    depends_on "lsof" => :test
+  end
 
   resource "com.openssh.sshd.sb" do
     url "https://opensource.apple.com/source/OpenSSH/OpenSSH-209.50.1/com.openssh.sshd.sb"
@@ -25,27 +30,29 @@ class ObfuscatedOpenssh < Formula
   end
 
   # Both these patches are applied by Apple.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a74/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
-    sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
+  if OS.mac?
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a745f1fe726900974845d1b0dd3c3398d6/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
+      sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
+    end
+
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2612fd251ac6de17bf0cc5174c3aab94c/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
+      sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
+    end
   end
 
   patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
-    sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
-  end
-
-  patch do
-    url "https://raw.githubusercontent.com/zinglau/obfuscated-openssh-patches/master/portable/8.1.diff"
-    sha256 "0b4eea2a1b5416f7555850b09c47fc2a41516d3ba97f6605e198b720eced3602"
+    url "https://raw.githubusercontent.com/zinglau/obfuscated-openssh-patches/master/portable/8.3.diff"
+    sha256 "58196218ccc5ddae53e646bf0160f2d300913c9184adc19324e3575f87ac12a3"
   end
 
   def install
-    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
+    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__" if OS.mac?
 
     # Ensure sandbox profile prefix is correct.
     # We introduce this issue with patching, it's not an upstream bug.
-    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
+    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh" if OS.mac?
 
     args = %W[
       --prefix=#{prefix}
@@ -55,7 +62,11 @@ class ObfuscatedOpenssh < Formula
       --with-kerberos5
       --with-pam
       --with-ssl-dir=#{Formula["openssl@1.1"].opt_prefix}
+      --with-security-key-builtin
     ]
+
+    args << "--with-pam" if OS.mac?
+    args << "--with-privsep-path=#{var}/lib/sshd" unless OS.mac?
 
     system "./configure", *args
     system "make"
