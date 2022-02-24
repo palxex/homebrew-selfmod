97b5fd903c23d16cbf28d24e48598f5aa8c5a040
palxex <ex@palx.org>
2022-02-24 14:24:50 +0800

    obfuscated-openssh: upgrade to 8.8

--- Formula/obfuscated-openssh.rb
+++ Formula/obfuscated-openssh.rb
@@ -1,17 +1,11 @@
 class ObfuscatedOpenssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools with obfuscated patch"
   homepage "https://www.openssh.com/"
-  url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.5p1.tar.gz"
-  mirror "https://mirror.vdms.io/pub/OpenBSD/OpenSSH/portable/openssh-8.5p1.tar.gz"
-  version "8.5p1"
-  sha256 "f52f3f41d429aa9918e38cf200af225ccdd8e66f052da572870c89737646ec25"
+  url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.8p1.tar.gz"
+  mirror "https://mirror.vdms.io/pub/OpenBSD/OpenSSH/portable/openssh-8.8p1.tar.gz"
+  version "8.8p1"
+  sha256 "4590890ea9bb9ace4f71ae331785a3a5823232435161960ed5fc86588f331fe9"
   license "SSH-OpenSSH"
-
-
-  livecheck do
-    url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/"
-    regex(/href=.*?openssh[._-]v?(\d+(?:\.\d+)+(?:p\d+)?)\.t/i)
-  end
 
   conflicts_with "openssh",
     :because => "both install ssh scp sshd etc"
@@ -23,21 +17,14 @@ class ObfuscatedOpenssh < Formula
   depends_on "ldns"
   depends_on "libfido2"
   depends_on "openssl@1.1"
-  
-  unless OS.mac?
-    depends_on "libedit"
-    depends_on "krb5"
-    depends_on "zlib"
-    depends_on "lsof" => :test
-  end
 
-  resource "com.openssh.sshd.sb" do
-    url "https://opensource.apple.com/source/OpenSSH/OpenSSH-209.50.1/com.openssh.sshd.sb"
-    sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
-  end
+  uses_from_macos "lsof" => :test
+  uses_from_macos "krb5"
+  uses_from_macos "libedit"
+  uses_from_macos "zlib"
 
-  # Both these patches are applied by Apple.
-  if OS.mac?
+  on_macos do
+    # Both these patches are applied by Apple.
     patch do
       url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a745f1fe726900974845d1b0dd3c3398d6/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
       sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
@@ -49,17 +36,28 @@ class ObfuscatedOpenssh < Formula
     end
   end
 
+  on_linux do
+    depends_on "linux-pam"
+  end
+
+  resource "com.openssh.sshd.sb" do
+    url "https://opensource.apple.com/source/OpenSSH/OpenSSH-240.40.1/com.openssh.sshd.sb"
+    sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
+  end
+
   patch do
-    url "https://raw.githubusercontent.com/zinglau/obfuscated-openssh-patches/master/portable/8.5.diff"
-    sha256 "eef6253ee0bed9fe083005b3012190f5736fa257e2e19537ddb1627b2109bb28"
+    url "https://raw.githubusercontent.com/zinglau/obfuscated-openssh-patches/master/portable/8.8.diff"
+    sha256 "e997cddf992bb95b2202c14b91daa2ee4e0df96882bc683a3ebb918c2eb70e50"
   end
 
   def install
-    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__" if OS.mac?
+    if OS.mac?
+      ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
 
-    # Ensure sandbox profile prefix is correct.
-    # We introduce this issue with patching, it's not an upstream bug.
-    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh" if OS.mac?
+      # Ensure sandbox profile prefix is correct.
+      # We introduce this issue with patching, it's not an upstream bug.
+      inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
+    end
 
     args = %W[
       --prefix=#{prefix}
@@ -67,12 +65,12 @@ class ObfuscatedOpenssh < Formula
       --with-ldns
       --with-libedit
       --with-kerberos5
+      --with-pam
       --with-ssl-dir=#{Formula["openssl@1.1"].opt_prefix}
       --with-security-key-builtin
     ]
 
-    args << "--with-pam" if OS.mac?
-    args << "--with-privsep-path=#{var}/lib/sshd" unless OS.mac?
+    args << "--with-privsep-path=#{var}/lib/sshd" if OS.linux?
 
     system "./configure", *args
     system "make"
@@ -86,6 +84,15 @@ class ObfuscatedOpenssh < Formula
 
     buildpath.install resource("com.openssh.sshd.sb")
     (etc/"ssh").install "com.openssh.sshd.sb" => "org.openssh.sshd.sb"
+  end
+  
+  test do
+    assert_match "OpenSSH_", shell_output("#{bin}/ssh -V 2>&1")
+
+    port = free_port
+    fork { exec sbin/"sshd", "-D", "-p", port.to_s }
+    sleep 2
+    assert_match "sshd", shell_output("lsof -i :#{port}")
   end
 
   plist_options :startup => true
@@ -112,20 +119,5 @@ class ObfuscatedOpenssh < Formula
     </dict>
     </plist>
   EOS
-  end
-
-  test do
-    if ENV["HOMEBREW_GITHUB_ACTIONS"]
-      # Fixes "Starting sshd: Privilege separation user sshd does not exist FAILED" in docker
-      system "groupadd", "-g", "133", "sshd"
-      system "useradd", "-u", "133", "-g", "133", "-c", "sshd", "-d", "/", "sshd"
-    end
-
-    assert_match "OpenSSH_", shell_output("#{bin}/ssh -V 2>&1")
-
-    port = free_port
-    fork { exec sbin/"sshd", "-D", "-p", port.to_s }
-    sleep 2
-    assert_match "sshd", shell_output("lsof -i :#{port}")
   end
 end
